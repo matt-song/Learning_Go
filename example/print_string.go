c944478df8adcae8021e86de6678ab886a22e4b7
Matt Song <matt.song@live.cn>
2019-06-04 14:01:21 +0800

    Updated at 2019-06-04

---
+++ example/print_string.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+     fmt.Println("calculating the floor of [2.75]")
+     fmt.Println("the result is:",math.Floor(2.75))
+     fmt.Println("\nConvert strings [fuck off asshole] to title...")
+     fmt.Println(strings.Title("fuck off asshole"))
+}
